ab3c77f2cec353c8c4961a7b06fc7d3a08128545
Jermaine Goins <tower2.24@gmail.com>
2015-10-08 19:17:44 -0500

    Initial Commit

---
+++ Super Cool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Cool
+//
+//  Created by Jermaine Lorenzo Goins on 10/8/15.
+//  Copyright © 2015 Jermaine Lorenzo Goins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
